1388cac11738543b6c6cd7dce763354cbea01355
Damian Baćkowski <damianbackowski@gmail.com>
2016-09-08 18:09:41 +0200

    change options

--- eu-cookies-notice.js
+++ eu-cookies-notice.js
@@ -1,15 +1,14 @@
 window.euCookiesNotice = (function($, Cookies) {
   'use strict';
 
-  var text = 'Ta strona korzysta z ciasteczek. Dalsze korzystanie ze strony oznacza, że zgadzasz się na ich użycie.';
-  var buttonText = 'Zamknij';
+  var options = {};
 
   var cookiesInfoTemplate = function() {
     var result = '<div id="eu-cookies-notice">';
     result += '<div class="container"><div class="row"><div class="col-sm-12"><span class="">';
-    result += text;
+    result += options.text;
     result += '</span>';
-    result += '<span class=""><a href="#" id="eu-cookies-notice-accept" class="btn btn-default btn-xs">' + buttonText + '</a></span>';
+    result += '<span class=""><a href="#" id="eu-cookies-notice-accept" class="btn btn-default btn-xs">' + options.buttonText + '</a></span>';
     result += '</div></div></div></div>';
 
     return result;
@@ -35,9 +34,11 @@ window.euCookiesNotice = (function($, Cookies) {
   }
 
   var euCookiesNotice = {
-    init: function(cookieNoticeText, cookieCloseButtonText) {
-      text = cookieNoticeText !== undefined ? cookieNoticeText : text;
-      buttonText = cookieCloseButtonText !== undefined ? cookieCloseButtonText : buttonText;
+    init: function(opts) {
+      options = $.extend({
+        text: 'Ta strona korzysta z ciasteczek. Dalsze korzystanie ze strony oznacza, że zgadzasz się na ich użycie.',
+        buttonText: 'Zamknij'
+      }, opts);
 
       if (noticeUnaccepted()) {
         showNotice();
